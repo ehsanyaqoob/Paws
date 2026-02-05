00bdf9de550e2bac238f0494ec0889c367310085
ehsanyaqoob <ehsanyaqoob07@gmail.com>
2026-02-05 10:23:38 +0500

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by ehsanyaqoob on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
